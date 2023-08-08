bce334e249e1c745a63b8cc156e8c935954b8260
Garrett Rathbone <garrettrathbone@users.noreply.github.com>
2023-08-08 10:29:19 -0500

    Update lib.py

--- src/rest_api_client/lib.py
+++ src/rest_api_client/lib.py
@@ -194,20 +194,16 @@ class RestAPI:
         return self._process_endpoint_response(call, response)
 
     def _process_endpoint_response(self, call: PreparedCall, response):
-        logger.debug(response.content)
-        logger.debug(response)
         logger.debug(call)
+        logger.debug(response)
         response.raise_for_status()
         try:
             json_response = response.json()
             if call.model:
-                logger.debug("model")
                 return call.model(**json_response)
-            logger.debug("no model")
             return json_response
         except Exception as e:
             logger.debug(e)
-            logger.debug(response)
             return response.text
 
     def _create_methods(self, endpoint: Endpoint):
